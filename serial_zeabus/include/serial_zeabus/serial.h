18369c4284f3beee4a663332e19921de8c0fb607
Supasan Komonlit <supasan_224@hotmail.com>
2018-08-11 02:34:12 +0700

    finish header

--- serial_zeabus/include/serial_zeabus/serial.h
+++ serial_zeabus/include/serial_zeabus/serial.h
@@ -53,4 +53,98 @@ namespace serial_zeabus{
 			}
 	}
 
+	class IO_error	:	public	std::runtime_error{
+		public:
+			io_error( const std::string& description ): std::runtime_error( description ){}
+	}
+
+// virtual is have affect about pointer can change destination to same return and name function
+	class package{
+		public:
+			package() {}
+
+			virtual ~package() {}
+
+			virtual	std::string toString() const = 0;
+			virtual void get_data_stream( serial_zeabus::data_stream& buffer) const = 0;
+	};
+
+	class package_error	:	public	std::runtime_error{
+		public:
+			package_error( const package &p, uint8_t code) : 
+				std::runtime_error( generate_string( p , code) ){}
+
+		private:
+			generate_string( const package &p , uint8_t code){
+				std::stringstream temporary;
+				temporary << p.toString();
+				return temporary.str();
+		}
+	}
+
+// boost::noncopyable is using protectd constructor and desrructor
+	class serial : private boost::noncopyable{
+
+		public:
+			enum{
+				IO_in_process,
+				IO_done,
+				IO_error,
+				IO_timeout,
+			};
+
+			serial();
+
+			virtual ~serial();
+
+			bool open_port(	std::string port , 
+							unsigned int baud_rate, 
+							boost::asio::serial_port_base::parity opt_parity =
+								boost::asio::serial_port_base::parity( 
+									boost::asio::serial_port_base::parity::none),
+
+							boost::asio::serial_port_base::character_size opt_csize = 
+								boost::asio::serial_port_base::character_size(8),
+
+							boost::asio::serial_port_base::flow_control opt_flow =
+								boost::asio::serial_port_base::flow_control(
+									boost::asio::serial_port::flow_control::none),
+
+							boost::asio::serial_port_base::stop_bits opt_stop = 
+								boost::asio::serial_port_base::stop_bits(
+									boost::asio::serial_port_base::stop_bits::one)
+							);
+
+			void close_port();
+
+			bool is_open();
+
+			void change_baud_rate( unsigned int baud_rate);
+
+			// to change to async_write
+			void write_package( const serial_zeabus::package& package ,  int timeout);
+
+			void write_string( const std::string& message);
+
+			// to make asyn and timeout supported
+			std::string readLine();
+
+			void write_data( const data_stream& data , int timeout );
+
+			virtual void read_handle( const boost::system::error_code& ec ,
+									  const size_t bytes_transferred);
+
+			virtual void timer_handle( const boost::system::error_code& ec);
+
+			//asyn read fixed amount of data
+			void async_read_block_of_data( data_stream& data , size_t size , int timeout);
+
+		protected:
+			boost::asio::io_service IO;
+			boost::asio::serial_port port;
+			boost::asio::deadline_time timer;
+			int byte_transferref;
+			int IO_state;
+	};
+
 }

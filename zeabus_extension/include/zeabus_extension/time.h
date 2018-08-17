23a7a17ba1af0c7735c773b2ddaa157f59382c92
Supasan Komonlit <supasan_224@hotmail.com>
2018-08-17 18:07:47 +0700

    finish edit

--- zeabus_extension/include/zeabus_extension/time.h
+++ zeabus_extension/include/zeabus_extension/time.h
@@ -13,7 +13,6 @@
 #include	<ctime> // use for local time
 #include	<boost/date_time/posix_time/posix_time.hpp> // for world time
 #include	<boost/date_time/posix_time/posix_time_duration.hpp> // for calculate diff
-#include	<boost/chrono/system_clocks.hpp> // for accuracy time
 #include	<zeabus_extension/convert_to_string.h> // for convert to string
 
 #include	<boost/thread/thread.hpp> //  for sleep
@@ -60,31 +59,41 @@ namespace count_time{
 			boost::posix_time::ptime time_01;
 			boost::posix_time::time_duration temporary;
 			void find_different();
+			bool print_test;
 
-	}
+	};
 
 	class second : public time{
 		public:
+			second( bool print_test );
+			second();
 			long different( bool reset );
-	}
+			void sleep( double data );
+	};
 
 	class millisecond : public time{
 		public:
+			millisecond( bool print_test );
+			millisecond();
 			long different( bool reset );
-	}
+			void sleep( double data );
+	};
 
 	class microsecond : public time{
 		public:
+			microsecond( bool print_test );
+			microsecond();
 			long different( bool reset );
-	}
+			void sleep( double data );
+	};
 
 	class nanosecond : public time{
 		public:
+			nanosecond( bool print_test );
+			nanosecond();
 			long different( bool reset );
-	}
+			void sleep( double data );
+	};
 
 }
 
-namespace timer{
-	void sleep( double data );
-}
---
+++ zeabus_extension/src/count_time.cpp
@@ -0,0 +1,101 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//	File's name		: count_time.cpp
+//
+//	Last Update		: Aug 17 , 2018
+//	Author			: Supasan Komonlit
+//
+//	Main purpose	: manage about time that want more accuracy
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include	<zeabus_extension/time.h>
+
+count_time::time::time( bool print_test ){
+	this->print_test = print_test;
+}
+
+count_time::time::time( ){
+	this->print_test = false;
+}
+
+void count_time::time::reset_time(){
+	time_01 = boost::posix_time::second_clock::local_time();
+}
+
+void count_time::time::find_different(){
+	temporary = boost::posix_time::second_clock::local_time() - time_01;
+}
+
+long count_time::second::different( bool reset){
+	this->find_different();
+	if( reset ) this->reset_time();
+	return temporary.total_seconds();
+}
+
+long count_time::millisecond::different( bool reset){
+	this->find_different();
+	if( reset ) this->reset_time();
+	return temporary.total_milliseconds();
+}
+
+long count_time::microsecond::different( bool reset){
+	this->find_different();
+	if( reset ) this->reset_time();
+	return temporary.total_microseconds();
+}
+
+long count_time::nanosecond::different( bool reset){
+	this->find_different();
+	if( reset ) this->reset_time();
+	return temporary.total_nanoseconds();
+}
+
+void count_time::second::sleep( double data ){
+	boost::this_thread::sleep( boost::posix_time::millisec( data * 1000 ) );
+}
+
+void count_time::millisecond::sleep( double data ){
+	boost::this_thread::sleep( boost::posix_time::millisec( data ) );
+}
+
+void count_time::microsecond::sleep( double data ){
+	boost::this_thread::sleep( boost::posix_time::microsec( data ) );
+}
+
+void count_time::nanosecond::sleep( double data ){
+	boost::this_thread::sleep( boost::posix_time::microsec( data / 1000 ) );
+}
+
+count_time::second::second( bool print_test ){
+	this->print_test = print_test;
+}
+
+count_time::second::second( ){
+	this->print_test = false;
+}
+
+count_time::millisecond::millisecond( bool print_test ){
+	this->print_test = print_test;
+}
+
+count_time::millisecond::millisecond( ){
+	this->print_test = false;
+}
+
+count_time::microsecond::microsecond( bool print_test ){
+	this->print_test = print_test;
+}
+
+count_time::microsecond::microsecond( ){
+	this->print_test = false;
+}
+
+count_time::nanosecond::nanosecond( bool print_test ){
+	this->print_test = print_test;
+}
+
+count_time::nanosecond::nanosecond( ){
+	this->print_test = false;
+}
+

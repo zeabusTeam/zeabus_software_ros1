03c3dae2f451242bd9a6329deb1848a69ba27878
AyumiizZ <korrawitc@gmail.com>
2018-09-12 00:31:09 +0700

    :sparkles: add convert function and get topic function

--- zeabus_vision/src/vision_lib.py
+++ zeabus_vision/src/vision_lib.py
@@ -1,11 +1,27 @@
-import color_text as ctxt
+import color_text as ct
 
-def print_result(msg, color=ctxt.DEFAULT):
+
+def print_result(msg, color=ct.DEFAULT):
     """
         print ('<----------') + str(msg) + ('---------->')
         #len of <---msg---> = 50
     """
-    print '<{:-^80}>'.format(' ' + color + str(msg) + ctxt.DEFAULT + ' ')
+    print '<{:-^80}>'.format(' ' + color + str(msg) + ct.DEFAULT + ' ')
+
 
 def img_is_none():
-    print(ctxt.RED + 'img is none.\nPlease check topic name or check camera is running' + ctxt.DEFAULT)
+    print(ct.RED + 'img is none.\nPlease check topic name or check camera is running' + ct.DEFAULT)
+
+
+def get_topic(camera):
+    if camera == 'front':
+        return '/vision/front/image_raw/compressed'
+    elif camera == 'bottom':
+        return '/vision/bottom/image_raw/compressed'
+
+
+def Aconvert(inp, full):
+    inp = float(inp)
+    full = float(full)
+    res = (inp - (full / 2.0)) / (full / 2.0)
+    return res

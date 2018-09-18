0f700eed1fcc4a45bf5e3bd2bd7ccbf93bbd95c1
Supasan Komonlit <supasan_224@hotmail.com>
2018-09-19 06:37:51 +0700

    init part of catkin and package

---
+++ zeabus_planner/scripts/manage_log.py
@@ -0,0 +1,9 @@
+#!/use/bin/env python2
+
+import rospy # import library of ros for python language
+import rospkg # get path to package in ROS
+
+
+class manage_log:
+# init part when declare class	
+	def __init__( self , name_file):

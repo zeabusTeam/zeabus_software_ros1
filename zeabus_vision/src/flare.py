2a6b29ffac1c8593ef94aea0e4ab81d200749efe
AyumiizZ <korrawitc@gmail.com>
2018-09-19 12:03:51 +0700

    :bug: edit khem bug

--- zeabus_vision/src/flare.py
+++ zeabus_vision/src/flare.py
@@ -45,36 +45,45 @@ def message(n_obj=0, cx=0.0, cy=0.0, area=0.0):
     print n_obj
     return msg
 
+
 def get_mask(img):
     hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
-    upper, lower = get_color_range('yellow', 'front', '1', 'flare')
+    # upper, lower = get_color_range('yellow', 'front', '1', 'flare')
+    upper = np.array([0, 0, 0], dtype=np.uint8)
+    lower = np.array([180, 255, 255], dtype=np.uint8)
     mask = cv.inRange(hsv, lower, upper)
     return mask
 
-def get_ROI(mask,case):
+
+def get_ROI(mask, case='near'):
     himg, wimg = mask.shape[:2]
     ROI = []
-    contours = cv.findContours(mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)[1]
+    contours = cv.findContours(
+        mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)[1]
     for cnt in contours:
-        check_area = 500
-        if case == 'near' :
-            check_area = 1500
-        if cv.contourArea(cnt) < check_area :
+        check_area = 500 if case == 'near' else 1500
+        area = cv.contourArea(cnt)
+        if area < check_area:
             continue
         x, y, w, h = cv.boundingRect(cnt)
-        top_excess = (y < 0.05*image_result)
+        # top_excess = (y < 0.05*image_result)
         right_excess = ((x+w) > 0.95*image_result)
         left_excess = (x < (0.05*image_result))
         bottom_excess = ((y+h) > 0.95*image_result)
-        if case == 'near' :
-            if (top_excess and  bottom_excess) and not(left_excess or right_excess) :
+        if case == 'near':
+            percent_area = (float(area)/(himg*wimg))
+            big_case = percent_area > 0.4 and bottom_excess
+            small_case = percent_area <= 0.4 and bottom_excess and not left_excess and not right_excess
+            if big_case or small_case:
                 ROI.append(cnt)
-        else :
+        elif case == 'far':
             h_w_ratio = 1.0*h/w
-            if not(left_excess or right_excess) and h_w_ratio >= h_w_ratio*50/100 and h_w_ratio <= h_w_ratio *150/100 :
+            if not(left_excess or right_excess) and h_w_ratio >= 2:
                 ROI.append(cnt)
     return ROI
-def get_cx(cnt) :
+
+
+def get_cx(cnt):
     global image_result
     himg, wimg = image_result.shape[:2]
     x, y, w, h = cv.boundingRect(cnt)
@@ -94,26 +103,24 @@ def find_near_flare():
         img_is_none()
         return message(n_obj=-1)
     mask = get_mask(image_result)
-    ROI = get_ROI(mask,case = 'near')
-    if len(ROI) == 0 :
-        mode = 1
-        print_result("NOT FOUND", color_text.RED)
-    elif len(ROI) == 1 :
-        mode = 2
-        print_result("FOUND A FLARE", color_text.GREEN)
-    elif len(ROI) > 1 :
-        mode = 2
-        print_result("FOUND BUT HAVE SOME NOISE", color_text.YELLOW)
-    if mode == 1 :
-        publish_result(image_result, 'bgr', pub_topic + 'image_result')
-        publish_result(mask, 'gray', pub_topic + 'mask')
+    ROI = get_ROI(mask, case='near')
+    mode = len(ROI)
+    if mode == 0:
+        print_result("NOT FOUND", ct.RED)
+        publish_result(image_result, 'bgr', public_topic + 'image_result')
+        publish_result(mask, 'gray', public_topic + 'mask')
         return message()
-    elif mode == 2:
-        cx, cy, area = get_cx(cnt = max(ROI,key=cv.contourArea))
-        publish_result(image_result, 'bgr', pub_topic + 'image_result')
-        publish_result(mask, 'gray', pub_topic + 'mask')
-        return message(cx=cx, cy=cy, area=area, n_obj =len(ROI))
-    
+    elif mode >= 1:
+        if mode == 1:
+            print_result("FOUND A FLARE", ct.GREEN)
+        elif mode > 1:
+            print_result("FOUND BUT HAVE SOME NOISE (" +
+                         str(mode) + ")", ct.YELLOW)
+        cx, cy, area = get_cx(cnt=max(ROI, key=cv.contourArea))
+        publish_result(image_result, 'bgr', public_topic + 'image_result')
+        publish_result(mask, 'gray', public_topic + 'mask')
+        return message(cx=cx, cy=cy, area=area, n_obj=len(ROI))
+
 
 def find_far_flare():
     global bgr
@@ -121,30 +128,30 @@ def find_far_flare():
         img_is_none()
         return message(n_obj=-1)
     mask = get_mask(image_result)
-    ROI = get_ROI(mask,case = 'far')
-     if len(ROI) == 0 :
+    ROI = get_ROI(mask, case='far')
+    if len(ROI) == 0:
         mode = 1
-        print_result("NOT FOUND", color_text.RED)
-    elif len(ROI) == 1 :
+        print_result("NOT FOUND", ct.RED)
+    elif len(ROI) == 1:
         mode = 2
-        print_result("FOUND A FLARE", color_text.GREEN)
-    elif len(ROI) > 1 :
+        print_result("FOUND A FLARE", ct.GREEN)
+    elif len(ROI) > 1:
         mode = 2
-        print_result("FOUND BUT HAVE SOME NOISE", color_text.YELLOW)
-    if mode == 1 :
-        publish_result(image_result, 'bgr', pub_topic + 'image_result')
-        publish_result(mask, 'gray', pub_topic + 'mask')
+        print_result("FOUND BUT HAVE SOME NOISE", ct.YELLOW)
+    if mode == 1:
+        publish_result(image_result, 'bgr', public_topic + 'image_result')
+        publish_result(mask, 'gray', public_topic + 'mask')
         return message()
     elif mode == 2:
-        cx, cy, area = get_cx(cnt = max(ROI,key=cv.contourArea))
-        publish_result(image_result, 'bgr', pub_topic + 'image_result')
-        publish_result(mask, 'gray', pub_topic + 'mask')
-        return message(cx=cx, cy=cy, area=area, n_obj =len(ROI))
+        cx, cy, area = get_cx(cnt=max(ROI, key=cv.contourArea))
+        publish_result(image_result, 'bgr', public_topic + 'image_result')
+        publish_result(mask, 'gray', public_topic + 'mask')
+        return message(cx=cx, cy=cy, area=area, n_obj=len(ROI))
 
 
 if __name__ == '__main__':
     rospy.init_node('vision_flare', anonymous=False)
-    
+
     image_topic = get_topic("front")
     rospy.Subscriber(image_topic, CompressedImage, image_callback)
     rospy.Service('vision_flare', vision_srv_flare(),
--- zeabus_vision/src/vision_lib.py
+++ zeabus_vision/src/vision_lib.py
@@ -1,3 +1,7 @@
+import rospy
+import numpy as np
+from cv_bridge import CvBridge 
+from sensor_msgs.msg import Image
 import color_text as ct
 
 
@@ -13,27 +17,52 @@ def img_is_none():
     print(ct.RED + 'img is none.\nPlease check topic name or check camera is running' + ct.DEFAULT)
 
 
+def publish_result(img, type, topicName):
+    """
+        publish picture
+    """
+    if img is None:
+        img = np.zeros((200, 200))
+        type = "gray"
+    bridge = CvBridge()
+    pub = rospy.Publisher(
+        str(topicName), Image, queue_size=10)
+    if type == 'gray':
+        msg = bridge.cv2_to_imgmsg(img, "mono8")
+    elif type == 'bgr':
+        msg = bridge.cv2_to_imgmsg(img, "bgr8")
+    pub.publish(msg)
+
+
 def get_topic(camera):
     if camera == 'front':
         return '/vision/front/image_raw/compressed'
     elif camera == 'bottom':
         return '/vision/bottom/image_raw/compressed'
 
-def get_color_range(color, camera_position, number, mission):
-    lower = None
-    upper = None
-    color_list = CONST.COLOR_LIST
-    print('color_range_' + str(camera_position) + '_' + str(number) + '_' + str(mission) + '/color_' + camera_position + '/lower_' + color)
-    if color in color_list:
-        lower = rospy.get_param(
-            'color_range_' + str(camera_position) + '_' + str(number) + '_' + str(mission) + '/color_' + camera_position + '/lower_' + color, '0,0,0')
-        upper = rospy.get_param(
-            'color_range_' + str(camera_position) + '_' + str(number) + '_' + str(mission) + '/color_' + camera_position + '/upper_' + color, '179,255,255')
-        lower = range_str2array(lower)
-        upper = range_str2array(upper)
-        print "FOUND"
-    print(lower, upper)
-    return lower, upper
+
+def range_str2array(string):
+    string = string.split(',')
+    return np.array([int(string[0]), int(string[1]), int(string[2])], dtype=np.uint8)
+
+
+# def get_color_range(color, camera_position, number, mission):
+#     lower = None
+#     upper = None
+#     color_list = CONST.COLOR_LIST
+#     print('color_range_' + str(camera_position) + '_' + str(number) +
+#           '_' + str(mission) + '/color_' + camera_position + '/lower_' + color)
+#     if color in color_list:
+#         lower = rospy.get_param(
+#             'color_range_' + str(camera_position) + '_' + str(number) + '_' + str(mission) + '/color_' + camera_position + '/lower_' + color, '0,0,0')
+#         upper = rospy.get_param(
+#             'color_range_' + str(camera_position) + '_' + str(number) + '_' + str(mission) + '/color_' + camera_position + '/upper_' + color, '179,255,255')
+#         lower = range_str2array(lower)
+#         upper = range_str2array(upper)
+#         print "FOUND"
+#     print(lower, upper)
+#     return lower, upper
+
 
 def Aconvert(inp, full):
     inp = float(inp)

4ef522150627688362e3874bae65b794331ed24c
Supasan Komonlit <supasan_224@hotmail.com>
2018-10-03 08:56:42 +0700

    init setup gate

--- zeabus_planner/scripts/sauvc_01_gate_v1.py
+++ zeabus_planner/scripts/sauvc_01_gate_v1.py
@@ -15,11 +15,9 @@ from zeabus_vision.msg import vision_gate
 
 class play_gate:
 
-	def __init__( self , rate , first_forward , first_survey , forward , survey ):
+	def __init__( self , rate ):
 		self.data_vision = { "color" : None  , "cx_01" : 0 , "cx_02" : 0 ,
 							 "cy_01" : 0 , "cy_02" : 0 , "area" : 0 }
-		self.navigation = { "first_forward" : first_forward , "first_survey" : first_survey,
-							"forward" : forwaard , "survey" : survey}
 
 		print("Waiting gate service")
 		rospy.wait_for_service('vision_gate')
@@ -27,7 +25,20 @@ class play_gate:
 		
 		self.rate = rospy.Rate( rate)
 
+		self.already_setup = False
+
+	def setup( self , first_forward , first_survey , forward , survey ):
 		
+		self.already_setup = True
+
+		self.navigation = { "first_forward" : first_forward , "first_survey" : first_survey,
+							"forward" : forwaard , "survey" : survey}
+
+		self.log_command = log( "zeabus_planner" , "log" , "02_gate_command")
+		self.log_vision = log( "zeabus_planner" , "log" , "02_gate_vision")
+		self.client_flare = rospy.ServiceProxy('vision_gate' , vision_srv_gate )
+
+	
 
 if __name__=='__main__':
 	rospy.init_node("Mission Gate")	

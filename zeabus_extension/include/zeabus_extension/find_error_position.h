a955a7346d886ee8ec525951ef109cbc6669ea05
Supasan Komonlit <supasan_224@hotmail.com>
2018-10-16 12:31:29 +0700

    Finish fine error position

--- zeabus_extension/include/zeabus_extension/find_error_position.h
+++ zeabus_extension/include/zeabus_extension/find_error_position.h
@@ -9,8 +9,6 @@
 //
 /////////////////////////////////////////////////////////////////////////////////////////////////
 
-#ifndef _zeabus_extension_find_error_poition_h__
-
 #ifndef IOSTREAM
 	#include	<iostream>
 	#define IOSTREAM
@@ -35,18 +33,20 @@
 	#define STDLIB_H
 #endif
 
+#define		CHECK_FIND_ERROR
+
 namespace zeabus_extension{
 
 namespace zeabus_control{
 
 	template<typename number_type> void convert_angular( number_type& point);
 
-	template<typename number_type> number_type find_distance( int8_t size
+	template<typename number_type> number_type find_distance( int8_t size 
 															, number_type* point_1
 															, number_type* point_2 );
 
-	template<typename number_type> void find_min_angular( number_type start 
-												, number_type target , number_type& result);
+	template<typename number_type> void find_min_angular( number_type& start 
+												, number_type& target , number_type& result);
 
 	template<typename number_type> void find_error_position( number_type *point_1 
 					, number_type *point_2 , number_type *result_type );
@@ -55,4 +55,3 @@ namespace zeabus_control{
 
 }
 
-#endif

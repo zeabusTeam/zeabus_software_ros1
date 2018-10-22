e0fa517364fa6c6aa37e163e421018f882096493
Supasan Komonlit <supasan_224@hotmail.com>
2018-10-22 12:29:31 +0700

    connect two mission

---
+++ zeabus_planner/scripts/sauvc_00_v1.py
@@ -0,0 +1,29 @@
+import rospy
+from sauvc_01_gate_v2 import play_gate
+from sauvc_02_flare_v1 import play_flare
+try:
+	from zeabus_extension.manage_log import log
+	from zeabus_extension.control_auv import control_auv 
+except:
+	print("Pleas install setup.bash in zeabus_extension package")
+	exit()
+
+if __name__=="__main__":
+	
+
+	rospy.init_node("Mission Planner")		
+	auv = control_auv("Connection")
+	gate = play_gate( 30 )
+	gate.setup( 4 , 4 , 1 , 2)
+	gate.play()
+
+	print("Finish play gate")
+	auv.relative_yaw( -1.57 )
+	rate = rospy.Rate(30)
+	while( not rospy.is_shutdown() and not auv.ok_position("yaw" , 0.1)):
+		rate.sleep()
+	print("Now play flare")
+
+	flare = play_flare( 30 )
+	flare.set_up()
+	flare.play()

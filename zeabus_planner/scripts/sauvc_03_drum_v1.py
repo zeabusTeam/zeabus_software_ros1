1bdc065ce77110f99937b227ae3e8dafd6b4f5f8
skconan <supakit.kr@gmail.com>
2018-10-30 00:26:55 +0700

    Update from robot

---
+++ zeabus_planner/scripts/sauvc_03_drum_v1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python2
+
+import rospy
+import math
+
+try:
+	from zeabus_extension.manage_log import log
+	from zeabus_extension.control_auv import control_auv 
+except:
+	print("Pleas install setup.bash in zeabus_extension package")
+	exit()
+
+from zeabus_vision.srv import vision_srv_drum
+from zeabus_vision.msg import vision_drum
+from std_msgs.msg import String
+
+

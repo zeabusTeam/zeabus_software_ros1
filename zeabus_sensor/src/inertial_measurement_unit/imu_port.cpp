6d6816ecb571a159ee009e87c19ec53a2b534b27
Supasan Komonlit <supasan_224@hotmail.com>
2018-12-04 11:00:20 +0700

    Delete old file

--- zeabus_sensor/src/inertial_measurement_unit/imu_port.cpp
+++ zeabus_sensor/src/inertial_measurement_unit/imu_port.cpp
@@ -21,6 +21,8 @@
 #include	<zeabus_library/convert_bytes.h>
 #include	"./../../../zeabus_library/src/convert_bytes.cpp"
 
+#define	_DEBUG_SPILT_DATA_
+
 namespace Asio = boost::asio;
 namespace DataIMU = zeabus_sensor::MIP_COMMUNICATION::DATA::IMU_DATA_SET ;
 
@@ -28,20 +30,34 @@ int main( int argv , char** argc ){
 	
 	ros::init( argv , argc , "main_control");
 
+	ros::NodeHandle nh(""); // Handle for manage about this file in ros system
 	ros::NodeHandle ph(""); // Handle for manage param from launch
 
-	std::string port_name = "/dev/microstrain/3dm_gx5_45_0000__6251.65901";
+////////////////////////////////////-- PARAMETER PART --/////////////////////////////////////////
+	std::string topic_output;
+	std::string port_name;
+	int frequency;
 
-	zeabus_sensor::LordMicrostrain imu( port_name );
+	ph.param< std::string >("name_port_imu" , port_name 
+								, "/dev/microstrain/3dm_gx5_45_0000__6521.65901");
 
-	ros::Rate rate( 500 );
+	ph.param< std::string >("topic_output_port_imu" , topic_output , "/sensor/imu/port");
+
+	ph.param< int >("frequency_imu" , frequency , 250 );
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+	ros::Rate rate( frequency );
 
 	ros::Publisher tell_data = 
-			ph.advertise< zeabus_library::IMUData >("/sensor/imu/port" , 1 );
+			ph.advertise< zeabus_library::IMUData >( topic_output , 1 );
 
 	zeabus_library::IMUData message;
 
 	bool result ;
+
+	zeabus_sensor::LordMicrostrain imu( port_name );
 
 	imu.open_port( result );
 
@@ -60,15 +76,15 @@ int main( int argv , char** argc ){
 		printf( "IMU set to ide ");
 		imu.command_idle( result );
 		rate.sleep();
-	}while( ( ! result ) && ph.ok() );
+	}while( ( ! result ) && nh.ok() );
 
 	do{
 		printf( "IMU get base rate ");
 		imu.sensor_get_IMU_base_rate( result , IMU_base_rate );
 		rate.sleep();
-	}while( ( ! result ) && ph.ok() );
+	}while( ( ! result ) && nh.ok() );
 
-	imu.set_IMU_rate( IMU_base_rate / 500 );
+	imu.set_IMU_rate( IMU_base_rate / frequency );
 
 	do{
 		printf( "Set type message ");
@@ -82,17 +98,17 @@ int main( int argv , char** argc ){
 	do{
 		printf( "Set setting format ");
 		imu.sensor_save_message_format( result );
-	}while( ( ! result ) && ph.ok() );
+	}while( ( ! result ) && nh.ok() );
 
 	do{
 		printf( "Set enable stream ");
 		imu.sensor_enable_data_stream( true , false , result );
-	}while( ( ! result ) && ph.ok() );
+	}while( ( ! result ) && nh.ok() );
 
 	do{
 		printf( "Set resume for data stream ");
 		imu.command_resume( result );	
-	}while( ( ! result ) && ph.ok() );
+	}while( ( ! result ) && nh.ok() );
 
 	std::vector< uint8_t > data_stream;
 	while( ph.ok() ){
@@ -101,25 +117,33 @@ int main( int argv , char** argc ){
 		if( result ){
 			for( int run = 4 ; run < data_stream.size() ; ){
 				if( data_stream[ run ] == DataIMU::SCALED_ACCELEROMETER_VECTOR ){
-					printf("Position %d Accelerometer--> %zX\n" , run, data_stream[run] );
+					#ifdef _DEBUG_SPILT_DATA_
+						printf("Position %d Accelerometer--> %zX\n" , run, data_stream[run] );
+					#endif
 					run += 1;
 					uint8_t_to_Point3( message.linear_acceleration , data_stream , run );
 					run += 12 ;
 				}
 				else if( data_stream[run] == DataIMU::SCALED_GYRO_VECTOR ){
-					printf("Position %d SCALED GYRO --> %zX\n" , run ,data_stream[run]);
+					#ifdef _DEBUG_SPILT_DATA_
+						printf("Position %d SCALED GYRO --> %zX\n" , run ,data_stream[run]);
+					#endif
 					run += 1;
 					uint8_t_to_Point3( message.angular_velocity , data_stream , run );
 					run += 12 ;
 				}
 				else if( data_stream[ run ] == DataIMU::CF_EULER_ANGLES ){
-					printf("Position %d EULER ANGLES --> %zX\n" ,run ,data_stream[run]);
+					#ifdef _DEBUG_SPILT_DATA_
+						printf("Position %d EULER ANGLES --> %zX\n" ,run ,data_stream[run]);
+					#endif
 					run += 1;
 					uint8_t_to_Point3( message.euler , data_stream , run );
 					run += 12;
 				}
 				else{
-					printf("Skip\n");
+					#ifdef _DEBUG_SPILT_DATA_
+						printf("Skip\n");
+					#endif
 					run += 1;
 				}
 			}

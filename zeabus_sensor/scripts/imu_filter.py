17047123e06f69f5c436649b2419040313a359f7
skconan <supakit.kr@gmail.com>
2018-12-04 22:35:43 +0800

    Prepare for imu monitoring

--- zeabus_sensor/scripts/imu_filter.py
+++ zeabus_sensor/scripts/imu_filter.py
@@ -4,6 +4,7 @@ import rospy
 import tf
 from sensor_msgs.msg import Imu
 import os
+import numpy as np
 PATH = os.path.dirname(os.path.abspath(__file__))
 print(PATH)
 
@@ -27,21 +28,23 @@ class CSVManager:
         pass 
 
 class SensorFusion:
-    def __init__(self):
-        rospy.init_node("SensorFusion")
+    def __init__(self,axis=0):
+        rospy.init_node("SensorFusion",anonymous=True)
         rospy.Subscriber("/imu/data",Imu, self.imu_callback)
-        rospy.Subscriber("/gx4_45_imu/data",Imu, self.old_imu_callback)
+        # rospy.Subscriber("/gx4_45_imu/data",Imu, self.old_imu_callback)
         
         self.orientation = None
         self.euler = [0,0,0]
-        self.old_velocity = [0,0,0]
-        self.velocity = [0,0,0]
-        self.csv = CSVManager(PATH + "/csv/compare_old_new.csv")
+        self.old_acc = [0,0,0]
+        self.acc = [[0],[0],[0]]
+        # self.csv = CSVManager(PATH + "/csv/compare_old_new.csv")
+        self.count = 0
+        self.axis = axis
 
     def imu_callback(self,msg):
-        self.velocity[0] = msg.linear_acceleration.x    
-        self.velocity[1] = msg.linear_acceleration.y    
-        self.velocity[2] = msg.linear_acceleration.z    
+        self.acc[0].append(msg.linear_acceleration.x)    
+        self.acc[1].append(msg.linear_acceleration.y)    
+        self.acc[2].append(msg.linear_acceleration.z)    
         # self.quaternion = (
         #     msg.orientation.x,
         #     msg.orientation.y,
@@ -50,17 +53,31 @@ class SensorFusion:
         # )
         # self.euler = tf.transformations.euler_from_quaternion(self.quaternion)
         # print(self.euler)
-
-    def old_imu_callback(self, msg):
-        self.old_velocity[0] = msg.linear_acceleration.x    
-        self.old_velocity[1] = msg.linear_acceleration.y    
-        self.old_velocity[2] = msg.linear_acceleration.z
         self.run()
 
-    def run(self):
-        self.csv.writer(self.velocity + self.old_velocity)
+    # def old_imu_callback(self, msg):
+    #     self.old_acc[0] = msg.linear_acceleration.x    
+    #     self.old_acc[1] = msg.linear_acceleration.y    
+    #     self.old_acc[2] = msg.linear_acceleration.z
 
+    def run(self):
+        if len(self.acc[self.axis]) > 0 and len(self.acc[self.axis]) % 100 == 0:
+            # print("Count: " + str(count*100)+"   Mean | Max | Min | Variance | Total Variance |")
+            mean100 = np.mean(self.acc[self.axis][self.count*100:self.count*100 + 100])
+            max100 = np.max(self.acc[self.axis][self.count*100:self.count*100 + 100])
+            min100 = np.min(self.acc[self.axis][self.count*100:self.count*100 + 100])
+            var100 = np.var(self.acc[self.axis][self.count*100:self.count*100 + 100])
+            var = np.var(self.acc[self.axis])
+            self.count += 1
+            if self.count % 20 == 0 and self.count > 0:
+                print("============================= Axis: %s ======================================"%self.axis)
+                print("%8s   %10s   %10s   %10s   %14s %14s" %("Count |", "Mean |", "Max |" ,"Min |" ,"Variance |", "Total Variance |"))
+            print("%6d |  %+7.6f |  %+7.6f |  %+7.6f |  %+9.10f |  %+9.10f |"%(self.count*100,mean100,max100,min100,var100,var))
 
 if __name__ == '__main__':
-    sf = SensorFusion()
+    axis = raw_input("Axis: ")
+    ax = {'x':0,'y':0,'z':0}
+    sf = SensorFusion(ax[axis.lower()])
+    print("============================= Axis: %s ======================================"%axis)
+    print("%8s   %10s   %10s   %10s   %14s %14s" %("Count |", "Mean |", "Max |" ,"Min |" ,"Variance |", "Total Variance |"))
     rospy.spin()

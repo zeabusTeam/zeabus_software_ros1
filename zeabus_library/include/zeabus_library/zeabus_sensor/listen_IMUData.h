279723f852b3db19104f9c16654fa347d2c6bc4b
Supasan Komonlit <supasan_224@hotmail.com>
2018-12-08 23:45:31 +0700

    Remian callback function

--- zeabus_library/include/zeabus_library/zeabus_sensor/listen_IMUData.h
+++ zeabus_library/include/zeabus_library/zeabus_sensor/listen_IMUData.h
@@ -20,7 +20,7 @@
 #include	<zeabus_library/IMUData.h>
 
 #include	<zeabus_library/convert_rotation.h>
-
+#include	<zeabus_library/convert_IMUData.h>
 #ifndef _ZEABUS_LIBRARY_ZEABUS_SENSOR_LISTEN_IMUDATA_H__
 #define _ZEABUS_LIBRARY_ZEABUS_SENSOR_LISTEN_IMUDATA_H__
 
@@ -29,14 +29,27 @@ namespace zeabus_sensor{
 	class ListenIMUData{
 
 		public:
-			ListenIMUData( double roll , double pitch , double yaw );
+			ListenIMUData( double roll = 0 , double pitch = 0, double yaw = 0
+							, double gravity = 0 );
 
-			void callback( const zeabus_library::IMUData& result );
+			void callback( const zeabus_library::IMUData& message );
 			void get_result( zeabus_library::IMUData& result );
 
+			void set_orientation( double roll , double pitch , double yaw );
+			void set_gravity( double gravity );
+
 		private:
+			// for collecting matrix transform
 			boost::numeric::ublas::matrix< double > matrix_imu_to_robot;
 			boost::numeric::ublas::matrix< double > matrix_imu_to_world;
+			// for collecting specific data
+			boost::numeric::ublas::matrix< double > offset_euler;
+			boost::numeric::ublas::matrix< double > offset_gravity;
+			// for collecting matrix receive data
+			boost::numeric::ublas::matrix< double > receive_euler;
+			boost::numeric::ublas::matrix< double > receive_gyro;
+			boost::numeric::ublas::matrix< double > receive_acceleration;
+			// for collecting matrix result for get data
 			boost::numeric::ublas::matrix< double > result_euler;
 			boost::numeric::ublas::matrix< double > result_gyro;
 			boost::numeric::ublas::matrix< double > result_acceleration;
--- zeabus_library/src/zeabus_sensor/listen_IMUData.cpp
+++ zeabus_library/src/zeabus_sensor/listen_IMUData.cpp
@@ -17,19 +17,44 @@
 
 namespace zeabus_sensor{
 
-	ListenIMUData::ListenIMUData( double roll , double pitch , double yaw ){
+	ListenIMUData::ListenIMUData( double roll , double pitch , double yaw , double gravity ){
 		this->matrix_imu_to_robot.resize( 3 , 3 ); // set size of matrix
 		this->matrix_imu_to_world.resize( 3 , 3 ); // set size of matrix
-		// find matrix to convert imu to robot so this is will have only one value
+		// find matrix to convert imu to robot so this is will have only one value	
 		this->matrix_handle.all_rotation(	roll	, pitch		, yaw 
 											, matrix_imu_to_robot );
+
 		// set size matrix for result when want to get value
 		this->result_euler.resize( 1 , 3 );
 		this->result_gyro.resize( 1 , 3 );
 		this->result_acceleration.resize( 1 , 3 );	
+
+		// srt size matrix for collecting specific value
+		this->offset_euler.resize( 1 , 3 );
+		this->offset_gravity.resize( 1 , 3 );
+		this->set_orientation( roll , pitch , yaw );
+		this->set_gravity( gravity );
+	}
+			
+	void ListenIMUData::callback( const zeabus_library::IMUData& message ){
+			
+	}
+
+	void set_orientation( double roll , double pitch , double yaw ){
+		this->offset_euler( 0 , 0 ) = roll ; 
+		this->offset_euler( 0 , 1 ) = pitch;
+		this->offset_euler( 0 , 2 ) = yaw;
+	}
+
+	void set_gravity( double gravity ){
+		this->offset_gravity( 0 , 0 ) = 0 ;
+		this->offset_gravity( 0 , 1 ) = 0 ;
+		this->offset_gravity( 0 , 2 ) = gravity ;
 	}
 
 	void ListenIMUData::get_result( zeabus_library::IMUData& message ){
+		split_matrix_to_IMUData( this->result_euler , this->result_gyro
+								,this->result_acceleration , message );		
 	}
 
 }

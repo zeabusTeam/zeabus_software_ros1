162a5ff0338988fe6e002e7261585fc301a94a17
Supasan Komonlit <supasan_224@hotmail.com>
2018-12-20 15:11:07 +0700

    Add error code pattern

---
+++ zeabus_library/include/zeabus_library/error_code.h
@@ -0,0 +1,27 @@
+/*
+	File name			:	error_code.h		
+	Author				:	Supasan Komonlit
+	Date created		:	2018 , DEC 20
+	Date last modified	:	2018 , ??? ??
+	Purpose				:	This header library for meaning and useing tell error code
+							
+	Maintainer			:	Supasan Komonlit
+	e-mail				:	supasan.k@ku.th
+	version				:	0.5.0
+	status				:	Production
+
+	Namespace			:	zeabus_library
+*/
+
+#ifndef _ZEABUS_LIBRARY_ERROR_CODE_H__
+#define _ZEABUS_LIBRARY_ERROR_CODE_H__
+
+namespace zeabus_library{
+	
+	static const uint16_t NO_ERROR = 0;
+	static const uint16_t ERROR_SIZE_ARRAY = 1;  
+	static const uint16_t ERROR_SIZE_MATRIX = 2;
+
+}
+
+#endif

fcd1db37097ca704fb0275d581d73de6389d56ee
Supasan Komonlit <supasan_224@hotmail.com>
2018-12-23 05:55:24 +0700

    Finish test set 1

--- zeabus_library/src_test/test_rotation_handle.cpp
+++ zeabus_library/src_test/test_rotation_handle.cpp
@@ -37,10 +37,10 @@ void result_test( int number_test , boost::numeric::ublas::matrix< double >& val
 	if( abs( result( 0 , 0 ) - expected( 0 , 0 ) ) <= EPSILON &&
 		abs( result( 1 , 0 ) - expected( 1 , 0 ) ) <= EPSILON &&
 		abs( result( 2 , 0 ) - expected( 2 , 0 ) ) <= EPSILON ){
-		printf("Test number %d is correct\n" , number_test );
+		printf("\nTest number %d is correct\n" , number_test );
 	}
 	else{
-		printf("Test number %d is wrong\n" , number_test );
+		printf("\nTest number %d is wrong\n" , number_test );
 		zeabus_library::vector::print( "Value before rotation" , value );
 		zeabus_library::vector::print( "Result after rotation" , result );
 		zeabus_library::vector::print( "Expected value after " , expected );
@@ -63,6 +63,7 @@ int main(){
 	boost::numeric::ublas::matrix< double > expected;
 	value.resize( 3 , 1 );
 	result.resize( 3 , 1 );
+	expected.resize( 3 , 1 );
 
 	std::cout << "Welcome to test zeabus_lirary test_rotation_handle\n";
 
@@ -75,29 +76,40 @@ int main(){
 	value( 1 , 0 ) = 0 ;
 	value( 2 , 0 ) = 1 ;
 
-	std::cout << "\nZero Test\n";
 	rotation_handle.set_target_frame( 0 , 0 , 0 );
 	rotation_handle.start_rotation( value , result );
 	input_value( expected , 0 , 0 , 1 );
 	result_test( 0 , value , result , expected );
 
-	std::cout << "\nFirst Test yaw is 90 \n";
 	rotation_handle.set_target_frame( 0 , 0 , degree( 90 ) );
 	rotation_handle.start_rotation( value , result );
 	input_value( expected , 0 , 0 , 1 );
 	result_test( 1 , value , result , expected );
 
-	std::cout << "\nSecond Test pitch 90 \n";
 	rotation_handle.set_target_frame( 0 , degree(90) , 0);
 	rotation_handle.start_rotation( value , result );
 	input_value( expected , -1 , 0 , 0 );
 	result_test( 2 , value , result , expected );
 
-	std::cout << "\nThird Test roll 90\n";
 	rotation_handle.set_target_frame( degree(90) , 0 , 0);
 	rotation_handle.start_rotation( value , result );
 	input_value( expected , 0 , 1 , 0 );
 	result_test( 3 , value , result , expected );
+
+	rotation_handle.set_target_frame( 0 , degree(45) , degree(45) );
+	rotation_handle.start_rotation( value , result );
+	input_value( expected , -0.707 , 0 , 0.707 );
+	result_test( 4 , value , result , expected );
+
+	rotation_handle.set_target_frame( degree(45) , degree(45) , degree(45) );
+	rotation_handle.start_rotation( value , result );
+	input_value( expected , -0.707 , 0.5 , 0.5 );
+	result_test( 5 , value , result , expected );
+
+	rotation_handle.set_target_frame( degree(0) , degree(180) , degree(0) );
+	rotation_handle.start_rotation( value , result );
+	input_value( expected , 0 , 0 , -1 );
+	result_test( 6 , value , result , expected );
 
 	return 0; 
 

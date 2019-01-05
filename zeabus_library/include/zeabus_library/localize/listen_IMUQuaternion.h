463a17a26911d81a16c4475d511a027a4035c072
Supasan Komonlit <supasan_224@hotmail.com>
2019-01-05 18:21:16 +0700

    I will surive

---
+++ zeabus_library/include/zeabus_library/localize/listen_IMUQuaternion.h
@@ -0,0 +1,25 @@
+/*
+	File name			:	listen_IMUQuaternion.h		
+	Author				:	Supasan Komonlit
+	Date created		:	2018 , DEC 29
+	Date last modified	:	2018 , ??? ??
+	Purpose				:	For count_time
+							
+	Maintainer			:	Supasan Komonlit
+	e-mail				:	supasan.k@ku.th
+	version				:	0.5.0
+	status				:	Product
+
+	Namespace			:	zeabus_library/localize
+*/
+
+#include	<ros/ros.h>
+
+#include	<stdio.h>
+
+#include	<zeabus_library/vector.h>
+
+#include	<zeabus_library/IMUQuaternion.h>
+
+#include	<zeabus_library/rotation/quaternion.h>
+

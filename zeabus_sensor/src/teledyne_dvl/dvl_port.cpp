5e732b554772092a99faa196b6648a4db089e3bc
Supasan Komonlit <supasan_224@hotmail.com>
2019-01-18 11:39:04 +0700

    Pass catkin dvl_port

--- zeabus_sensor/src/teledyne_dvl/dvl_port.cpp
+++ zeabus_sensor/src/teledyne_dvl/dvl_port.cpp
@@ -21,6 +21,10 @@
 
 #include	<zeabus_library/error_code.h>
 
+#include	<zeabus_library/file.h>
+
+#include	<zeabus_library/Point3.h>
+
 #include	<zeabus_library/sensor/string_port.h>
 
 #define _COLLECTING_DATA_
@@ -44,9 +48,9 @@ int main( int argc , char ** argv ){
 
 	#ifdef _COLLECTING_DATA_
 		zeabus_library::File log_file( "zeabus_log" , "sensor/dvl" , "dvl_port" );
-		size_t result;
-		result = log_file.open();
-		if( !( result == zeabus_library::NO_ERROR ) ) return zeabus_library::ERROR_ACTION;
+		size_t result_file;
+		result_file = log_file.open();
+		if( !( result_file == zeabus_library::NO_ERROR ) ) return zeabus_library::ERROR_ACTION;
 	#endif
 
 	zeabus_library::sensor::StringPort serial_port( port_name ) ;
@@ -101,21 +105,22 @@ int main( int argc , char ** argv ){
 ////////////////////////////////////////////////////////////////////////////////////////////////
 
 	std::string data;
-	char status;	
+	char status;
+	int temp_x , temp_y , temp_z;	
 
 	while( nh.ok() ){
 		serial_port.read_data( data );
 		#ifdef _COLLECTING_DATA_
-			log_file.wirteline( data );
+			log_file.writeline( data );
 		#endif
 		if( data.find(":BS") != std::string::npos ){
-			sscanf( data.c_str() , ":BS,%d,%d,%d,%c" , &( data_ros.x ) , &( data_ros.y )
-													, &( data_ros.y) , &status );
+			sscanf( data.c_str() , ":BS,%d,%d,%d,%c" , &( temp_x ) , &( temp_y )
+													, &( temp_z) , &status );
 			if( status == 'A' ){
 				printf( "<-------- DVL GOOD DATA ---------->\n");
-				data_ros.x * 0.001;
-				data_ros.y * 0.001;
-				data_ros.z * 0.001;
+				data_ros.x = temp_x * 0.001;
+				data_ros.y = temp_y * 0.001;
+				data_ros.z = temp_z * 0.001;
 				tell_dvl.publish( data_ros );
 			}
 			else{

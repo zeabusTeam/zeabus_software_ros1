4b5baaac03be01280e393462cb6637f1d979364c
Supasan Komonlit <supasan_224@hotmail.com>
2019-01-19 17:57:34 +0700

    Change topic output

--- zeabus_sensor/scripts/pressure_filter.py
+++ zeabus_sensor/scripts/pressure_filter.py
@@ -15,7 +15,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 queue = []
-pub = rospy.Publisher("/barometer/data", Odometry, queue_size=1)
+#pub = rospy.Publisher("/barometer/data", Odometry, queue_size=1)
+pub = rospy.Publisher("/elec/pressure/node", Odometry, queue_size=1)
 # pub_float = rospy.Publisher("/baro/float64", Float64, queue_size=90)
 count = 0
 r = 0.05

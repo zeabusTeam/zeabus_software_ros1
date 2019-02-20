69b33348558feb9c1946b0fd3d1d28ddae717b39
Supasan Komonlit <supasan_224@hotmail.com>
2019-02-20 21:31:35 +0700

    Add new package mission package

---
+++ zeabus_mission/scripts/control_connection.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python2
+#################################################################################################
+####
+####	FILE		: control_connection.py
+####	Maintain	: Supasan Komonlit
+####	Create on	: 2019 , FEB 20
+####	Purpose		: For connection with control part
+####
+#################################################################################################
+from __future__ import print_function
+#===============>
+
+import rospy
+import math
+
+from zeabus_library.srv import OneVector3Stamped , TwoStringVector3Stamped , ThreeOdometry
+
+################################### DETAILED OF SERVICE ########################################
+##	OneVector3Stamped
+##	TwoStringVector3Stamped
+##	ThreeOdometry
+

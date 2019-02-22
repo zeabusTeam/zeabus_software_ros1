6ba891788f9facba05a1cf145d7ab8fd401b8a9b
skconan <supakit.kr@gmail.com>
2019-02-22 19:36:28 +0700

    test estimate distance in drum mission

--- zeabus_vision/src/estimate_distance.py
+++ zeabus_vision/src/estimate_distance.py
@@ -13,7 +13,10 @@ import matplotlib.pyplot as plt
 from sensor_msgs.msg import CompressedImage
 from nav_msgs.msg import Odometry
 import math
+import rospy
 from operator import itemgetter
+from vision_lib import *    
+from geometry_msgs.msg import Point
 
 bgr = None 
 sub_sampling = 0.5
@@ -31,15 +34,22 @@ def image_callback(msg):
 def pressure_callback(msg):
     global depth, sub_sampling
     depth = msg.pose.pose.position.z
-    # print(depth)
 
 def estimate_distance():
     global bgr,depth
-   
+    position = Point()
+    
+
+    pub = rospy.Publisher('/vision/estimate_distance', Point, queue_size=10)
+
     while not rospy.is_shutdown():
         if bgr is None:
             continue
-
+        else:
+            position.x = 100
+            position.z = 100
+            position.y = 100
+        
         gray = cv.cvtColor(bgr, cv.COLOR_BGR2GRAY)
         bg = cv.medianBlur(gray, 61)
         fg = cv.medianBlur(gray, 5)
@@ -51,8 +61,8 @@ def estimate_distance():
         sub_pos = normalize(sub_pos)
         sub_neg = normalize(sub_neg)
 
-        cv.imshow('sub_pos',sub_pos)
-        cv.imshow('sub_neg',sub_neg)
+        publish_result(sub_pos,'gray','sub_pos')
+        # publish_result(sub_neg,'gray','sub_neg')
 
         _, obj = cv.threshold(
             sub_pos, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU
@@ -62,6 +72,10 @@ def estimate_distance():
             obj.copy(), cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE
         )
         display = cv.cvtColor(sub_neg.copy(), cv.COLOR_GRAY2BGR)
+        bg = np.uint8(bg)
+        fg = np.uint8(fg)
+
+
         r = 0
         circle = []
         for cnt in contours:
@@ -70,8 +84,12 @@ def estimate_distance():
             center = (int(x),int(y))
             radius = radius
             area_cir = math.pi*(radius**2)
+            
+            if area_cnt < 400:
+                continue
             if area_cir <= 0 or area_cnt/area_cir < 0.8:
                 continue
+            print(area_cnt)
             cv.circle(display,center,int(radius),(255,0,0),-1)
             circle.append([x,y,radius])
        
@@ -86,31 +104,34 @@ def estimate_distance():
             print("radius",radius)
             print("pixel_per_cm",pixel_per_cm)
             print("depth",depth)
-            if True:
-                x_distance_pixel = row/2.-y
-                y_distance_pixel = col/2.-x
-                print("row col",row,col)
-                print("x y",x,y)
-                print("x_distance_pixel y_distance_pixel:",x_distance_pixel,y_distance_pixel)
-                x_distance_cm = float(x_distance_pixel) / pixel_per_cm
-                y_distance_cm = float(y_distance_pixel) / pixel_per_cm
-                print("x_distance_cm:",x_distance_cm)
-                print("y_distance_cm:",y_distance_cm)
-                x_distance_meter = x_distance_cm/100.
-                y_distance_meter = y_distance_cm/100.
-                print("x_meter:",x_distance_meter)   
-                print("y_meter:",y_distance_meter)   
-                cv.circle(display,(int(x),int(y)),int(radius),(0,255,255),-1)
+                
+            x_distance_pixel = row/2.-y
+            y_distance_pixel = col/2.-x
+            print("row col",row,col)
+            print("x y",x,y)
+            print("x_distance_pixel y_distance_pixel:",x_distance_pixel,y_distance_pixel)
+            x_distance_cm = float(x_distance_pixel) / pixel_per_cm
+            y_distance_cm = float(y_distance_pixel) / pixel_per_cm
+            print("x_distance_cm:",x_distance_cm)
+            print("y_distance_cm:",y_distance_cm)
+            x_distance_meter = x_distance_cm/100.
+            y_distance_meter = y_distance_cm/100.
+            print("x_meter:",x_distance_meter)   
+            print("y_meter:",y_distance_meter)   
+            cv.circle(display,(int(x),int(y)),int(radius),(0,255,255),-1)
+            position.x = x_distance_meter
+            position.y = y_distance_meter
 
-        cv.imshow('obj',obj)
+        pub.publish(position)
+
+        publish_result(obj,'gray','obj')
         
-        bg = np.uint8(bg)
-        fg = np.uint8(fg)
+        publish_result(bgr,'bgr','original_bgr')
+        publish_result(bg,'gray','bg')
+        publish_result(fg,'gray','fg')
+        publish_result(display,'bgr','display')
 
-        cv.imshow('original_bgr', bgr)
-        cv.imshow('bg', bg)
-        cv.imshow('fg', fg)
-        cv.imshow('display', display)
+        rospy.sleep(1)
 
         k = cv.waitKey(1) & 0xff
         if k == ord('q'):
@@ -120,8 +141,8 @@ def estimate_distance():
 
 
 if __name__ == '__main__':
-    rospy.init_node('vision_gate', anonymous=False)
+    rospy.init_node('vision_estimate_distance', anonymous=False)
     image_topic = "/vision/bottom/image_raw/compressed"
     rospy.Subscriber(image_topic, CompressedImage, image_callback) 
-    # rospy.Subscriber("/sensor/pressure", Odometry, pressure_callback) 
+    rospy.Subscriber("/sensor/pressure", Odometry, pressure_callback) 
     estimate_distance()
